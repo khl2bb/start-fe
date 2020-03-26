871a3a32d0df209af073a9286167dc8d110528b3
이진우 <khl2bb@naver.com>
2020-03-27 01:32:07 +0900

    01 수업 과제
    3월 27일

---
+++ 01/app.js
@@ -0,0 +1,49 @@
+console.log('app.js')
+
+
+// let name = inoo;
+
+// let name = woo;
+
+// let count = 1;
+// console.log(count)
+// console.log(count++)
+// console.log(++count)
+
+// var log = function(str) {
+//     console.log(str);
+//   };
+
+  function log(str) {
+    console.log(str);
+  }
+//   log(1)
+//   log(log)
+//   log(tog)
+
+// var image = {}; // image = new Object();
+
+// image.width = 1;
+// image.height = 2;
+
+// image['width'] = 2; //연관배열
+
+// // image['name' + i] = '1.jpg'; //동적으로도 할당 가능
+// log(image)
+// image = { width: 1, height: 2 };
+
+// log(image)
+// let isWorking = false;
+// // log(isWorking)
+
+// var image = { width: 1, height: 2 };
+
+// console.log('width' in image); //true
+// console.log('valueOf' in image);
+// console.log('toString' in image);
+// log(image)
+
+var arr = { a:1};
+
+console.log(arr instanceof Object); //true
+
