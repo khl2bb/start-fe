40d11b13547f6453896a2f75b5a585c933f51f15
이진우 <khl2bb@naver.com>
2020-04-16 13:35:18 +0900

    04 과제 피드백 2
    html, js 파일 분리작업입니다.
    감사합니다.

---
+++ 04/quiz2.js
@@ -0,0 +1,36 @@
+var board = document.getElementById("board");
+
+function drawBoard() {
+    str = "";
+    str += '<div class="wrapper">'
+    for(var i=0; i<2; i++){
+        str += '<div class="floor"><div id="' + (i*7 + i+1) + '" class="black"> </div> <div id="' + (i*7 + i+2) + '" class="white"> </div> <div id="' + (i*7 + i+3) + '" class="black"> </div> <div id="' + (i*7 + i+4) + '" class="white"> </div> </div>';
+        str += '<div class="floor"><div id="' + (i*7 + i+5) + '" class="white"> </div> <div id="' + (i*7 + i+6) + '" class="black"> </div> <div id="' + (i*7 + i+7) + '" class="white"> </div> <div id="' + (i*7 + i+8) + '" class="black"> </div> </div>';
+    }
+    str += '</div>'
+    board.innerHTML = str;
+}
+    
+function log(str) {
+    console.log(str);
+}
+
+drawBoard();
+
+board.addEventListener("click", function(event) {
+    // log(event)
+    if(tempId = event.target.id){
+        drawBoard();
+        let targetBox = document.getElementById(tempId);
+        targetBox.style="background-color:red";
+    }  
+});
+
+// 마우스 클릭이 제대로 안 이루어질때
+board.addEventListener("mouseup", function(event) {
+    if(tempId = event.target.id){
+        drawBoard();
+        let targetBox = document.getElementById(tempId);
+        targetBox.style="background-color:red";
+    }
+});
