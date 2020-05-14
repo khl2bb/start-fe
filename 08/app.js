8077983ab98d98bcc63adac5bc111ba154233cd9
이진우 <khl2bb@naver.com>
2020-05-14 22:49:18 +0900

    08 디렉토리 업로드 입니다.
    http
    fetch
    재밌습니다.

---
+++ 08/app.js
@@ -0,0 +1,20 @@
+const promise = fetch('https://daum.net');
+console.log(promise);
+
+promise.then(res => {
+    console.log(res);
+    const textPromise = res.text();
+    console.log('textPromise:', textPromise);
+
+    textPromise.then((
+        data => console.log(data)
+
+
+    ));
+})
+console.log(d)
+
+
+function callback(data) {
+    console.log(data);
+}
