a266c64dd1c6315431e5d05d8762ad6939872a8e
이노마 <khl2bb@naver.com>
2020-05-28 13:22:57 +0900

    10 디렉토리 생성, webpack 실습
    10 디렉토리 생성, webpack 실습입니다.
    간단히 따라해보았습니다.

---
+++ 10/src/main.js
@@ -0,0 +1,6 @@
+import sumModule from './sum';
+
+// console.log(sumModule(10, 112));
+
+document.getElementById("debug").innerHTML = sum(1,2);
+document.body.innerHTML = subModule(1,23);
---
+++ 10/webpack.config.js
@@ -0,0 +1,15 @@
+// const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'app.js',
+    path: `${__dirname}/dist`,
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [new HtmlWebpackPlugin({
+      
+  })]
+}
