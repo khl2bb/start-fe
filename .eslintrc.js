ebaae85f2f5b85811fe26518e61cf18556bcc314
이진우 <khl2bb@naver.com>
2020-06-19 00:12:56 +0900

    13 count 실습 완료
    13 count 실습 완료

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": ["eslint:recommended", 'airbnb-base'],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    React: true,
+    ReactDOM: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
 };
